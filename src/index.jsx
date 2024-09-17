318dce848e564c0d7820116cc201a724a86e09ad
sanjaykumar <sanjay@tappetbox.com>
2024-09-17 16:31:43 +0530

    code

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./assets/scss/main.css";
 import "./assets/scss/File.scss";
+import TagManager from "react-gtm-module";
 
 import { Provider } from "react-redux";
 import store from "./store";
